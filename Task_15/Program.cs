43b7de456b4c706df409829f5755dbde9e23b8d6
Sveleb81 <sveleb81@gmail.com>
2022-12-04 16:23:13 +0300

    Task-15

---
+++ Task_15/Program.cs
@@ -0,0 +1,33 @@
+﻿int day = 6;
+
+if (day > 5 && day < 8) {
+    Console.Write(day);
+    Console.WriteLine(" -> да");
+} else {
+    Console.Write(day);
+    Console.WriteLine(" -> нет");
+}
+
+// 15-2
+
+day = 7;
+
+if (day > 5 && day < 8) {
+    Console.Write(day);
+    Console.WriteLine(" -> да");
+} else {
+    Console.Write(day);
+    Console.WriteLine(" -> нет");
+}
+
+// 15-3
+
+day = 1;
+
+if (day > 5 && day < 8) {
+    Console.Write(day);
+    Console.WriteLine(" -> да");
+} else {
+    Console.Write(day);
+    Console.WriteLine(" -> нет");
+}
