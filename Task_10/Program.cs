36a786bbb3fabe4063edd99072c5aef8f35e76e4
Sveleb81 <sveleb81@gmail.com>
2022-12-04 15:38:46 +0300

    Task-10

---
+++ Task_10/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {4, 5, 6};
+
+Console.WriteLine (array [1]);
+
+//Task 10-2
+
+  int [] array1 = {7, 8, 2};
+
+Console.WriteLine (array1 [1]);
+
+//Task 10-3
+
+ int [] array2  = {9, 1, 8};
+
+Console.WriteLine (array2 [1]);
+
