773c94e6753d6adb4078236c1713f39c6ee425de
Sveleb81 <sveleb81@gmail.com>
2022-12-04 16:18:22 +0300

    Task-13

---
+++ Task_13/Program.cs
@@ -0,0 +1,33 @@
+﻿int N = 645;
+
+string anyNumberText = Convert.ToString(N);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
+
+// 13-2
+
+N = 78;
+
+anyNumberText = Convert.ToString(N);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
+
+// 13-3
+
+N = 32679;
+
+anyNumberText = Convert.ToString(N);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
