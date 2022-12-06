bb361c7f1095f65ecee97ed418546577abc7e1d8
Sveleb81 <sveleb81@gmail.com>
2022-12-06 22:10:51 +0300

    Task_23

---
+++ Task_23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+for (int i = 1; i <= N; i++) {
+  double cube = Math.Pow(i, 3);
+  Console.Write(cube);
+  if (i != N) Console.Write(", ");
+}
