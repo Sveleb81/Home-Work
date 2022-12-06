0559ea996febaf2bdee80887c5f7fdbcd883f11e
Sveleb81 <sveleb81@gmail.com>
2022-12-06 21:43:36 +0300

    Task_19

---
+++ Task_19/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+string N = Console.ReadLine();
+
+if (N[0] == N[4] && N[1] == N[3]) {
+  Console.WriteLine("Введенное число является палиндромом");
+} else {
+  Console.WriteLine("Введенное число не является палиндромом");
+}
