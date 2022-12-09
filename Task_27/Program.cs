b19e9f8bd11bb4e5412981ef7bdecb437e7af98e
Sveleb81 <sveleb81@gmail.com>
2022-12-09 13:29:55 +0300

    Task_27

---
+++ Task_27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+string N = Console.ReadLine();
+
+int sum = 0;
+for (int i = 0; i < N.Length; i++) {
+    sum += Convert.ToInt16(Convert.ToString(N[i]));
+}
+
+Console.Write("Сумма чисел в числе: ");
+Console.Write(sum);
