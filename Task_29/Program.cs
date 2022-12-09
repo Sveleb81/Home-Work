ea1f8cffd87cbe2a935392d8566a7f5d697ce6c0
Sveleb81 <sveleb81@gmail.com>
2022-12-09 13:44:00 +0300

    Task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число элементов в массиве: "); 
+int N = Convert.ToInt32(Console.ReadLine());
+
+int [] arr = new int[N];
+
+for (int i = 0; i < N; i++) {
+    Console.Write(i + 1);
+    Console.Write(" элемент массива: ");
+    int el = Convert.ToInt32(Console.ReadLine());
+    arr[i] = el;
+}
+Console.Write("[");
+Console.Write(String.Join(", ", arr));
+Console.Write("]");
