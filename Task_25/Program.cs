ee2910f28fa55e8f3360eb076c90edbb7a52d2f9
Sveleb81 <sveleb81@gmail.com>
2022-12-09 13:16:21 +0300

    Task_25

---
+++ Task_25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Pow(A, B);
+
+Console.Write("A^B = ");
+Console.Write(result);
