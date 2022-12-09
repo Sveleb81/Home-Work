71cd7a2367a0922696c690fdf8e43683b2a1f567
Sveleb81 <sveleb81@gmail.com>
2022-12-09 14:30:20 +0300

    Task_38

---
+++ Task_38/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите число элементов в массиве: "); 
+int arrLength = Convert.ToInt32(Console.ReadLine());
+
+double [] arr = new double[arrLength];
+
+for (int i = 0; i < arrLength; i++) {
+    Console.Write($"{i + 1} элемент массива: ");
+    double el = Convert.ToDouble(Console.ReadLine());
+    arr[i] = el;
+}
+
+Console.WriteLine($"Массив: [{String.Join(", ", arr)}]");
+
+double distanceBetweenMaxAndMin(int arrSize, double [] arr) {
+    double max = arr[0];
+    double min = arr[0];
+
+    for (int i = 1; i < arrSize; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    return max - min;
+}
+
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива: {distanceBetweenMaxAndMin(arrLength, arr)}");
