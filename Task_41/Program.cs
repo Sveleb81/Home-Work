4820f79f2118545ccccf52fb7c8026389b0fa7c5
Sveleb81 <sveleb81@gmail.com>
2022-12-16 15:05:24 +0300

    Task_41

---
+++ Task_41/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+double [] arr = new double[M];
+
+for (int i = 0; i < M; i++) {
+    Console.Write($"{i + 1} число: ");
+    arr[i] = Convert.ToDouble(Console.ReadLine());
+}
+
+int getPositiveElementsCount(double [] arr, int arrLength) {
+  int count = 0;
+
+  for (int i = 0;  i < arrLength; i++) {
+    if (arr[i] > 0) count++;
+  }
+
+  return count;
+}
+
+Console.WriteLine($"Чисел больше нуля: {getPositiveElementsCount(arr, M)}");
