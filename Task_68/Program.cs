0b6d07c4ea15688774268922a1871c5f5130eeee
Sveleb81 <sveleb81@gmail.com>
2022-12-21 15:55:34 +0300

    Task_68

---
+++ Task_68/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите m: ");
+long m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите n: ");
+long n = Convert.ToInt32(Console.ReadLine());
+
+long Akk(long m, long n) {
+    if (m == 0) return n + 1;
+
+    if (m > 0 && n == 0) return Akk(m - 1, 1);
+
+    return Akk(m - 1, Akk(m, n - 1));
+}
+
+Console.Write($"A(m,n) = {Akk(m, n)}");
+
