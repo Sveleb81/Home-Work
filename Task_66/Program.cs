23f8f14c130e536043cf459ca7b59bbb5dd32806
Sveleb81 <sveleb81@gmail.com>
2022-12-21 15:41:19 +0300

    Task_66

---
+++ Task_66/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int getSumInRange(int start, int end, int sum) {
+    if (start == end) return sum + end;
+
+    return getSumInRange(start, end - 1, end + sum);
+}
+
+int sum = M > N ? getSumInRange(N, M, 0) : getSumInRange(M, N, 0);
+Console.Write($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+
