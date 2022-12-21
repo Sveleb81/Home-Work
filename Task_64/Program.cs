9c3a42616cb08dc2f60f8eae9601ca7c17a185b6
Sveleb81 <sveleb81@gmail.com>
2022-12-21 15:30:37 +0300

    Task_64

---
+++ Task_64/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void outputNaturalNumbersUpToN(int N) {
+    if (N == 1) {
+        Console.Write($"{N}");
+        return;
+    };
+
+    Console.Write($"{N}, ");
+    outputNaturalNumbersUpToN(N - 1);
+}
+
+outputNaturalNumbersUpToN(N);
